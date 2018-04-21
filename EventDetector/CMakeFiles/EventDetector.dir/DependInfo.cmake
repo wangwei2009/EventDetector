135914231465a44d25b5ba99375d0a4f5318f4e8
wangwei2009 <373955482ww@gmail.com>
2018-04-21 11:21:38 +0800

    modified cmakelist file

---
+++ EventDetector/CMakeFiles/EventDetector.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/EventDetector.cpp" "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/CMakeFiles/EventDetector.dir/EventDetector.cpp.o"
+  "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/File.cpp" "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/CMakeFiles/EventDetector.dir/File.cpp.o"
+  "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/TestLocalFile.cpp" "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/CMakeFiles/EventDetector.dir/TestLocalFile.cpp.o"
+  "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/feature_aggregator.cpp" "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/CMakeFiles/EventDetector.dir/feature_aggregator.cpp.o"
+  "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/feature_normalizer.cpp" "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/CMakeFiles/EventDetector.dir/feature_normalizer.cpp.o"
+  "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/fft_window.cpp" "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/CMakeFiles/EventDetector.dir/fft_window.cpp.o"
+  "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/main.cpp" "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/CMakeFiles/EventDetector.dir/main.cpp.o"
+  "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/mel_basis.cpp" "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/CMakeFiles/EventDetector.dir/mel_basis.cpp.o"
+  "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/model.cpp" "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/CMakeFiles/EventDetector.dir/model.cpp.o"
+  "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/postprocessing.cpp" "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/CMakeFiles/EventDetector.dir/postprocessing.cpp.o"
+  "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/preprocessing.cpp" "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/CMakeFiles/EventDetector.dir/preprocessing.cpp.o"
+  "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/wavread.cpp" "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/CMakeFiles/EventDetector.dir/wavread.cpp.o"
+  "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/weights.cpp" "/home/wangwei/QtProjects/Github/EventDetector/EventDetector/CMakeFiles/EventDetector.dir/weights.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "../alsautils"
+  "../eigen"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ EventDetector/CMakeFiles/EventDetector.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EventDetector.dir/main.cpp.o"
+  "CMakeFiles/EventDetector.dir/feature_aggregator.cpp.o"
+  "CMakeFiles/EventDetector.dir/feature_normalizer.cpp.o"
+  "CMakeFiles/EventDetector.dir/fft_window.cpp.o"
+  "CMakeFiles/EventDetector.dir/mel_basis.cpp.o"
+  "CMakeFiles/EventDetector.dir/weights.cpp.o"
+  "CMakeFiles/EventDetector.dir/model.cpp.o"
+  "CMakeFiles/EventDetector.dir/postprocessing.cpp.o"
+  "CMakeFiles/EventDetector.dir/wavread.cpp.o"
+  "CMakeFiles/EventDetector.dir/preprocessing.cpp.o"
+  "CMakeFiles/EventDetector.dir/TestLocalFile.cpp.o"
+  "CMakeFiles/EventDetector.dir/EventDetector.cpp.o"
+  "CMakeFiles/EventDetector.dir/File.cpp.o"
+  "bin/EventDetector.pdb"
+  "bin/EventDetector"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EventDetector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
